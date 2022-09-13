0e5068729eabc2991c201632c9ec0ea87da93361
AugmentVista <AuAgments@gmail.com>
2022-09-13 09:18:19 -0300

    emptied file folder

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Mr.world, do you take cream or sugar with your coffe or a pita");
+            Console.ReadKey(true); 
+
+
+        }
+    }
+}
